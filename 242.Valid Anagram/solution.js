bb37dedd844b33feb49f90eb6afc2960dbe12afe
tamilvanan <tamilvanansambasivam@gmail.com>
2023-11-21 19:53:01 +0530

    solution

---
+++ 242.Valid Anagram/solution.js
@@ -0,0 +1,12 @@
+var isAnagram = (s, t) => {
+  const isEqual = s.length === t.length;
+  if (!isEqual) return false;
+
+  return reorder(s) === reorder(t);
+};
+
+const reorder = (str) =>
+  str
+    .split("")
+    .sort((a, b) => a.localeCompare(b))
+    .join("");
