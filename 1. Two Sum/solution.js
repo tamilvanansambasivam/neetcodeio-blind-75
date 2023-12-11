dff23509a0ffc0333c8030ace3cd82514f204b76
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-11 08:26:17 +0530

    Update solution.js

--- 1. Two Sum/solution.js
+++ 1. Two Sum/solution.js
@@ -1,22 +1,15 @@
 /**
- * Hash Map - 1 Pass
- * Time O(N) | Space O(N)
- * https://leetcode.com/problems/two-sum/
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = (nums, target, map = new Map()) => {
-    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-        const num = nums[index];
-        const complement = (target - num);
-        const sumIndex = map.get(complement);
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
 
-        const isTarget = map.has(complement)
-        if (isTarget) return [ index, sumIndex ];
-
-        map.set(num, index);                                /* Space O(N) */
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == target - nums[j]) {
+                return [i, j]
+            }
+        }
     }
-
-    return [ -1, -1 ];
-}
+};
