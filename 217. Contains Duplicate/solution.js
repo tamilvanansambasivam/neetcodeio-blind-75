03be2f50779207b28a4464d782f6f6e0797c2b19
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-27 20:40:57 +0530

    Update solution.js

--- 217. Contains Duplicate/solution.js
+++ 217. Contains Duplicate/solution.js
@@ -1,18 +1,8 @@
-// var containsDuplicate = (nums) => {
-//   const numsSet = new Set(nums);
-//   const isEqual = numsSet.size === nums.length;
-
-//   return !isEqual;
-// };
-
-
-
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = (nums) => {
-    numsSet = new Set()
+var containsDuplicate = (nums, numsSet = new Set()) => {
     for (const num of nums) {/* Time O(N) */
         if (numsSet.has(num)) return true;
 
