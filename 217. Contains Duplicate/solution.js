c408f0913cc3bb33f699d4e2e2ca637b2a719c30
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-01-15 16:37:13 +0530

    Update solution.js

--- 217. Contains Duplicate/solution.js
+++ 217. Contains Duplicate/solution.js
@@ -2,23 +2,10 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-// Brute-force solution (may cause "Time Limit Exceeded" for large arrays)
-// This solution sorts the array and checks for duplicates using nested loops.
-// Sorting has a time complexity of O(n log n), and the nested loop has a time
-// complexity of O(n^2), resulting in an overall time complexity of O(n^2 log n).
-// For large arrays, this can lead to Time Limit Exceeded issues.
-// Consider alternative approaches or optimizations for better performance.
-var containsDuplicate = function(nums) {
-    var nums2 = nums.sort((a, b) => a - b);
-    console.log(nums2);
-
-    for (let i = 0; i < nums2.length - 1; i++) {
-        for (let j = i + 1; j < nums2.length; j++) {
-            if (nums2[i] === nums2[j]) {
-                return true;
-            }
-        }
+var containsDuplicate = function (nums) {
+    var nums2 = new Set(nums)
+    if (nums.length !== nums2.size) {
+        return true;
     }
-
     return false;
 };
