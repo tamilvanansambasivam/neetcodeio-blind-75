bb83a0db05b3a08ff181e9e58288d4f231fa2c8d
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-01-15 20:25:07 +0530

    format solution.js

--- 217. Contains Duplicate/solution.js
+++ 217. Contains Duplicate/solution.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const s = new Set(nums); 
+var containsDuplicate = function (nums) {
+    const s = new Set(nums);
     return s.size !== nums.length
 };
