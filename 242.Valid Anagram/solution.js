6f77601cecf2e165eeec9b76e2fff4dcad709e11
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-01-17 18:44:37 +0530

    Update solution.js

--- 242.Valid Anagram/solution.js
+++ 242.Valid Anagram/solution.js
@@ -3,6 +3,6 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {
-  return s.split("").sort().join("") === t.split("").sort().join("");
+var isAnagram = function(s, t) {
+    return s.split('').sort().join('') == t.split('').sort().join('');
 };
