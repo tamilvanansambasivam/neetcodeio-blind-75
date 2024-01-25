3b3c51e2b245643a6c9ccd56f08c3a918732cff6
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-01-25 13:19:27 +0530

    Update solution.js

--- 217. Contains Duplicate/solution.js
+++ 217. Contains Duplicate/solution.js
@@ -2,7 +2,23 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {
-    const s = new Set(nums);
-    return s.size !== nums.length
-};
+var containsDuplicate = (nums) => {
+    nums.sort((a, b) => a - b);/* Time O(N * log(N)) | Space O(1 || log(N)) */
+
+    return hasDuplicate(nums);
+}
+
+const hasDuplicate = (nums) => {
+    for (let curr = 0; curr < (nums.length); curr++) {/* Time O(N) */
+        const next = curr + 1;
+
+        if (nums[curr] === nums[next]) {
+            return nums[curr] === nums[next]
+
+        }
+
+    }
+
+    return false;
+}
+
