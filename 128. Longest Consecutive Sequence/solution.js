ac4c430add80415b414941b50cf2cce720748307
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-11 19:36:58 +0530

    Update solution.js

--- 128. Longest Consecutive Sequence/solution.js
+++ 128. Longest Consecutive Sequence/solution.js
@@ -7,23 +7,25 @@
  * @return {number}
  */
 var longestConsecutive = (nums, maxScore = 0) => {
-    const numSet = new Set(nums);         /* Time O(N) | Space O(N) */
+  const numSet = new Set(nums); /* Time O(N) | Space O(N) */
 
-    for (const num of [...numSet]) {    /* Time O(N) */
-        const prevNum = num - 1;
+  for (const num of [...numSet]) {
+    /* Time O(N) */
+    const prevNum = num - 1;
 
-        if (numSet.has(prevNum)) continue;/* Time O(N) */
+    if (numSet.has(prevNum)) continue; /* Time O(N) */
 
-        let [currNum, score] = [num, 1];
+    let [currNum, score] = [num, 1];
 
-        const isStreak = () => numSet.has(currNum + 1)
-        while (isStreak()) {              /* Time O(N) */
-            currNum++;
-            score++;
-        }
-
-        maxScore = Math.max(maxScore, score);
+    const isStreak = () => numSet.has(currNum + 1);
+    while (isStreak()) {
+      /* Time O(N) */
+      currNum++;
+      score++;
     }
 
-    return maxScore;
-}
+    maxScore = Math.max(maxScore, score);
+  }
+
+  return maxScore;
+};
