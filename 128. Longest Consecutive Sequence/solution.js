5c5eb424be0be52b4fb4d9de480c53bd66bff57f
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-11 19:36:03 +0530

    Update solution.js

--- 128. Longest Consecutive Sequence/solution.js
+++ 128. Longest Consecutive Sequence/solution.js
@@ -1,23 +1,29 @@
+/**
+ * Hash Set - Intelligent Sequence
+ * Greedy - Max Score
+ * Time O (N) | Space O(N)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
 var longestConsecutive = (nums, maxScore = 0) => {
-  const numSet = new Set(nums); /* Time O(N) | Space O(N) */
+    const numSet = new Set(nums);         /* Time O(N) | Space O(N) */
 
-  for (const num of [...numSet]) {
-    /* Time O(N) */
-    const prevNum = num - 1;
+    for (const num of [...numSet]) {    /* Time O(N) */
+        const prevNum = num - 1;
 
-    if (numSet.has(prevNum)) continue; /* Time O(N) */
+        if (numSet.has(prevNum)) continue;/* Time O(N) */
 
-    let [currNum, score] = [num, 1];
+        let [currNum, score] = [num, 1];
 
-    const isStreak = () => numSet.has(currNum + 1);
-    while (isStreak()) {
-      /* Time O(N) */
-      currNum++;
-      score++;
+        const isStreak = () => numSet.has(currNum + 1)
+        while (isStreak()) {              /* Time O(N) */
+            currNum++;
+            score++;
+        }
+
+        maxScore = Math.max(maxScore, score);
     }
 
-    maxScore = Math.max(maxScore, score);
-  }
-
-  return maxScore;
-};
+    return maxScore;
+}
