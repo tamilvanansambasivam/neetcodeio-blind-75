cde94fb0f38cfb11a0a7ebfce79a74bfa4574d7e
tamilvanan <tamilvanansambasivam@gmail.com>
2024-05-15 14:31:52 +0530

    leetcode solution

---
+++ 125. Valid Palindrome/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Array - Filter && Clone && Reverse
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  if (!s.length) return true;
+
+  const alphaNumeric = filterAlphaNumeric(s); /* Time O(N) | Space O(N) */
+  const reversed = reverse(alphaNumeric); /* Time O(N) | Space O(N) */
+
+  return alphaNumeric === reversed;
+};
+
+const filterAlphaNumeric = (
+  s,
+  nonAlphaNumeric = new RegExp("[^a-z0-9]", "gi")
+) =>
+  s
+    .toLowerCase() /* Time O(N) | Space O(N) */
+    .replace(nonAlphaNumeric, ""); /* Time O(N) | Space O(N) */
+
+const reverse = (s) =>
+  s
+    .split("") /* Time O(N) | Space O(N) */
+    .reverse() /* Time O(N) | Space O(N) */
+    .join(""); /* Time O(N) | Space O(N) */
