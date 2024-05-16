40ebd2cc3ba41edbca44b72e0a0d0fe91ef82edb
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-16 21:42:42 +0530

    Update solution.js

--- 125. Valid Palindrome/solution.js
+++ 125. Valid Palindrome/solution.js
@@ -1,29 +1,9 @@
 /**
- * Array - Filter && Clone && Reverse
- * Time O(N) | Space O(N)
- * https://leetcode.com/problems/valid-palindrome/
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-  if (!s.length) return true;
-
-  const alphaNumeric = filterAlphaNumeric(s); /* Time O(N) | Space O(N) */
-  const reversed = reverse(alphaNumeric); /* Time O(N) | Space O(N) */
-
-  return alphaNumeric === reversed;
+var isPalindrome = function(s) {
+    const clean = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+    const reversed = clean.split('').toReversed().join('');
+    return clean === reversed;
 };
-
-const filterAlphaNumeric = (
-  s,
-  nonAlphaNumeric = new RegExp("[^a-z0-9]", "gi")
-) =>
-  s
-    .toLowerCase() /* Time O(N) | Space O(N) */
-    .replace(nonAlphaNumeric, ""); /* Time O(N) | Space O(N) */
-
-const reverse = (s) =>
-  s
-    .split("") /* Time O(N) | Space O(N) */
-    .reverse() /* Time O(N) | Space O(N) */
-    .join(""); /* Time O(N) | Space O(N) */
