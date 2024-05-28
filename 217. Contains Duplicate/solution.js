65edc4983c9b394b769adfb43cde3be5b9250037
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-28 14:08:14 +0530

    Update solution.js

--- 217. Contains Duplicate/solution.js
+++ 217. Contains Duplicate/solution.js
@@ -1,16 +1,12 @@
 /**
- * Hash Set - Early Exit
- * Time O(N) | Space O(N)
- * https://leetcode.com/problems/contains-duplicate/
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = (nums, numsSet = new Set()) => {
-    for (const num of nums) {/* Time O(N) */
-        if (numsSet.has(num)) return true;
-
-        numsSet.add(num);       /* Space O(N) */
+var containsDuplicate = function (nums) {
+    let setNums = new Set(nums)
+    if (nums.length == [...setNums].length) {
+        return false;
+    } else {
+        return true
     }
-
-    return false;
 };
