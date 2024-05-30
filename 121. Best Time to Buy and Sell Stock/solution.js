63514382bdc1f53435a9e60f3bb159c52672e633
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-30 15:19:22 +0530

    Update solution.js

--- 121. Best Time to Buy and Sell Stock/solution.js
+++ 121. Best Time to Buy and Sell Stock/solution.js
@@ -7,19 +7,18 @@
  */
 
 var maxProfit = function (prices) {
-    let min = prices[0];
-    // min = 7
-    let max = min;
-    //max = 7
-    let value = 0;
-    for (let i = 0; i < prices.length; i++) {
-        if (i != prices.length - 1 && prices[i] <= min) {
-
-            max = min = prices[i];
-        } else if (prices[i] > max) {
-            max = prices[i];
-        }
-        value = max - min > value ? max - min : value; //5
+  let min = prices[0];
+  // min = 7
+  let max = min;
+  //max = 7
+  let value = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (i != prices.length - 1 && prices[i] <= min) {
+      max = min = prices[i];
+    } else if (prices[i] > max) {
+      max = prices[i];
     }
-    return value;
+    value = max - min > value ? max - min : value; //5
+  }
+  return value;
 };
