aa73f4c325f760766dee2ab0ded970c0bf066aee
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-05-30 15:18:23 +0530

    Update solution.js

--- 121. Best Time to Buy and Sell Stock/solution.js
+++ 121. Best Time to Buy and Sell Stock/solution.js
@@ -1,21 +1,25 @@
 /**
+ * Another approach without using sliding window
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  * Time O(N) | Space O(1)
  * @param {number} prices
  * @return {number}
  */
+
 var maxProfit = function (prices) {
-  let [left, right, max] = [0, 1, 0];
+    let min = prices[0];
+    // min = 7
+    let max = min;
+    //max = 7
+    let value = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (i != prices.length - 1 && prices[i] <= min) {
 
-  while (right < prices.length) {
-    const canSlide = prices[right] <= prices[left];
-    if (canSlide) left = right;
-
-    const window = prices[right] - prices[left];
-
-    max = Math.max(max, window);
-    right++;
-  }
-
-  return max;
+            max = min = prices[i];
+        } else if (prices[i] > max) {
+            max = prices[i];
+        }
+        value = max - min > value ? max - min : value; //5
+    }
+    return value;
 };
