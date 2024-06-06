f8728b031ab8b041d9f08e9d22e81d5582653f99
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-06-06 18:54:16 +0530

    Update solution.js

--- 242.Valid Anagram/solution.js
+++ 242.Valid Anagram/solution.js
@@ -4,5 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    return t.split("").sort().join("") == s.split("").sort().join("")
+  if (s.length !== t.length) {
+    return false;
+  }
+  let freq1 = {};
+  let freq2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    freq1[s[i]] = (freq1[s[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    freq2[t[i]] = (freq2[t[i]] || 0) + 1;
+  }
+
+  if (Object.keys(freq1).length == Object.keys(freq2).length) {
+    for (const property in freq2) {
+      if (freq1[property] != freq2[property]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 };
