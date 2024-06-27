22222897ac378076e4b06438d492fc020a8f0a9f
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2024-06-27 20:51:57 +0530

    Update solution.js

--- 49. Group Anagrams/solution.js
+++ 49. Group Anagrams/solution.js
@@ -1,26 +1,19 @@
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
 var groupAnagrams = function (strs) {
-    let map = {};
-    for (let i = 0; i < strs.length; i++) {
-        let sortedValue = sortedValueFun(strs[i])
+  let map = {};
+  for (let i = 0; i < strs.length; i++) {
+    let sortedValue = sortedValueFun(strs[i]);
 
-
-        if (map[sortedValue]) {
-            map[sortedValue].push(strs[i])
-        } else {
-            map[sortedValue] = []
-            map[sortedValue].push(strs[i])
-        }
+    if (map[sortedValue]) {
+      map[sortedValue].push(strs[i]);
+    } else {
+      map[sortedValue] = [];
+      map[sortedValue].push(strs[i]);
     }
+  }
 
-    return [...Object.values(map)]
+  return [...Object.values(map)];
 };
 
 function sortedValueFun(strs) {
-    return strs.split("").sort().join("")
+  return strs.split("").sort().join("");
 }
-
-
